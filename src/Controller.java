9d3421efc4ffa8e6e5eaa2ff391f3812d4205b01
joshuadwightred <42832831+joshuadwightred@users.noreply.github.com>
2022-12-22 22:24:13 +0100

    -Optimized imports

--- src/Controller.java
+++ src/Controller.java
@@ -1,13 +1,11 @@
 import javafx.application.Platform;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.*;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.util.Callback;
 
 import java.net.URL;
 import java.util.ResourceBundle;
